89fd70b599b1046e3fecd933802dea809cf60393
Mario Alberto <marioalberto220298@hotmail.com>
2020-06-25 17:39:44 -0500

    Caja de Comentarios

---
+++ CasaDeOracion/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CasaDeOracion.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+
+
+            return View();
+        }
+
+
+
+        public ActionResult events()
+        {
+            return View();
+        }
+        public ActionResult sermon()
+        {
+            return View();
+        }
+        public ActionResult blog()
+        {
+            return View();
+        }
+    }
+}
